a8bdd8a91e4ebb3aebdbc457541cf8e41892e052
WhiteHatTux <WhiteHatTux@timmch.de>
2015-08-30 17:03:41 +0200

    Add language-specific properties

--- src/main/java/de/timmch/zanata/App.java
+++ src/main/java/de/timmch/zanata/App.java
@@ -1,5 +1,8 @@
 package de.timmch.zanata;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        boolean lang_de = true;
+        Locale locale;
+        if ( lang_de ) {
+            locale = new Locale("de", "DE");
+        } else {
+            locale = new Locale("en", "US");
+        }
+        ResourceBundle labels = ResourceBundle.getBundle("messages", locale);
+
+        System.out.println(labels.getString("hello.world"));
+        System.out.println(labels.getString("good.bye.world"));
     }
 }
