7441d6e0f7fdc5764db0f872d9e38a165743e095
WhiteHatTux <WhiteHatTux@timmch.de>
2015-09-01 00:31:26 +0200

    Add a check for all languages
    This will show the default values for spanish until the translation
    is added.

--- src/main/java/de/timmch/zanata/App.java
+++ src/main/java/de/timmch/zanata/App.java
@@ -12,15 +12,26 @@ public class App
     public static void main( String[] args )
     {
         boolean lang_de = true;
+        String[] languages = new String[3];// = ;//, "de_DE", "en_US"];
+        languages[0] = "en_US";
+        languages[1] = "de_DE";
+        languages[2] = "es_ES";
         Locale locale;
         if ( lang_de ) {
-            locale = new Locale("de", "DE");
+            locale = new Locale("es", "ES");
         } else {
             locale = new Locale("en", "US");
         }
-        ResourceBundle labels = ResourceBundle.getBundle("messages", locale);
+        for (int i = 0; i < languages.length; i++) {
+            String shortLang = languages[i].split("_")[0];
+            String shortCountry = languages[i].split("_")[1];
+            locale = new Locale(shortLang, shortCountry);
+            ResourceBundle labels = ResourceBundle.getBundle("messages", locale);
 
-        System.out.println(labels.getString("hello.world"));
-        System.out.println(labels.getString("good.bye.world"));
+            System.out.println("This is the language with the abbreviation: " + shortLang);
+            System.out.println(labels.getString("hello.world"));
+            System.out.println(labels.getString("good.bye.world"));
+        }
+
     }
 }
